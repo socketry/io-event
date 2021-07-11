45d46656f54c05abbdd99344414a3d905968743c
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-07-11 12:08:55 +1200

    Update test for io-buffer branch.

--- test.rb
+++ test.rb
@@ -9,15 +9,16 @@ local, remote = UNIXSocket.pair
 selector = Event::Backend::URing.new(Fiber.current)
 
 f1 = Fiber.new do
-	buffer = String.new
-	length = selector.io_read(Fiber.current, local, buffer, 0, 1024)
-	pp ["f1", length, buffer, buffer.bytesize]
+	buffer = IO::Buffer.new(128)
+	length = selector.io_read(Fiber.current, local, buffer, 1)
+	pp ["f1", length, buffer]
 end
 
 f2 = Fiber.new do
-	buffer = "Hello World"
-	length = selector.io_write(Fiber.current, remote, buffer, 0, buffer.bytesize)
-	pp ["f2", length, buffer, buffer.bytesize]
+	buffer = IO::Buffer.new(128)
+	offset = buffer.copy("Hello World", 0)
+	length = selector.io_write(Fiber.current, remote, buffer, offset)
+	pp ["f2", length, buffer]
 end
 
 f1.transfer
