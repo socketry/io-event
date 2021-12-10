92a63dabe9d3bfb15b395f9ddef1ef556924da77
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-12-10 18:16:47 +1300

    Increase durations to improve robustness of test.

--- test/io/event/selector.rb
+++ test/io/event/selector.rb
@@ -64,13 +64,13 @@ Selector = Sus::Shared("a selector") do
 	with '#wakeup' do
 		it "can wakeup selector from different thread" do
 			thread = Thread.new do
-				sleep 0.05
+				sleep 0.1
 				selector.wakeup
 			end
 			
 			expect do
-				selector.select(0.2)
-			end.to have_duration(be < 0.1)
+				selector.select(0.5)
+			end.to have_duration(be < 0.5)
 		ensure
 			thread.join
 		end
