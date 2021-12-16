be7a93651557c81c408b24c35e339128c074c8ed
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2021-12-16 19:26:33 +1300

    Extended testing for `io_read`/`io_write`.

--- test/io/event/selector/file_io.rb
+++ test/io/event/selector/file_io.rb
@@ -24,37 +24,33 @@ require_relative '../../../environment'
 
 require 'io/event'
 require 'io/event/selector'
-require 'socket'
+require 'tempfile'
 
-BufferedIO = Sus::Shared("buffered io") do
-	with 'a pipe' do
-		let(:pipe) {IO.pipe}
-		let(:input) {pipe.first}
-		let(:output) {pipe.last}
+FileIO = Sus::Shared("file io") do
+	with 'a file' do
+		let(:file) {Tempfile.new}
 		
 		it "can read using a buffer" do
 			writer = Fiber.new do
 				buffer = IO::Buffer.new(128)
-				expect(selector.io_write(Fiber.current, output, buffer, 128)).to be == 128
+				file.seek(0)
+				expect(selector.io_write(Fiber.current, file, buffer, 128)).to be == 128
 			end
 			
 			reader = Fiber.new do
 				buffer = IO::Buffer.new(64)
-				expect(selector.io_read(Fiber.current, input, buffer, 1)).to be == 64
+				file.seek(0)
+				expect(selector.io_read(Fiber.current, file, buffer, 1)).to be == 64
 			end
 			
-			reader.transfer
 			writer.transfer
-			
-			expect(selector.select(1)).to be == 1
+			reader.transfer
 		end
 	end
 end
 
 IO::Event::Selector.constants.each do |name|
 	klass = IO::Event::Selector.const_get(name)
-	
-	next unless klass.instance_methods.include?(:io_read)
 	
 	describe(klass, unique: name) do
 		def before
@@ -69,6 +65,6 @@ IO::Event::Selector.constants.each do |name|
 		attr :loop
 		attr :selector
 		
-		it_behaves_like BufferedIO
+		it_behaves_like FileIO
 	end
 end
