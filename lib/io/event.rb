878591bd0eca4790ae4175d3bc329a0530c3f6e6
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-03 15:29:16 +0900

    Add priority heap and timers implementation. (#100)

--- lib/io/event.rb
+++ lib/io/event.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 # Released under the MIT License.
-# Copyright, 2021-2023, by Samuel Williams.
+# Copyright, 2021-2024, by Samuel Williams.
 
 require_relative 'event/version'
 require_relative 'event/selector'
+require_relative 'event/timers'
 
 begin
 	require 'IO_Event'
