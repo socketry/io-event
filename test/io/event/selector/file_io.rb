795d143c115b314880f6a455e7b180b7b0f55a92
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-10-05 13:14:04 +1300

    Better testing for io_uring.

--- test/io/event/selector/file_io.rb
+++ test/io/event/selector/file_io.rb
@@ -14,32 +14,51 @@ FileIO = Sus::Shared("file io") do
 		it "can read using a buffer" do
 			skip_if_ruby_platform(/mswin|mingw|cygwin/)
 			
+			write_result = nil
+			read_result = nil
+			
 			writer = Fiber.new do
 				buffer = IO::Buffer.new(128)
 				file.seek(0)
-				expect(selector.io_write(Fiber.current, file, buffer, 128)).to be == 128
+				write_result = selector.io_write(Fiber.current, file, buffer, 128)
 			end
 			
 			reader = Fiber.new do
 				buffer = IO::Buffer.new(64)
 				file.seek(0)
-				expect(selector.io_read(Fiber.current, file, buffer, 1)).to be == 64
+				read_result = selector.io_read(Fiber.current, file, buffer, 0)
 			end
 			
+			# The seek and the read/write are potentially racing since we use the same file descriptor. So we wait for the write to complete, and then wait for the read to complete.
+			
 			writer.transfer
+			
+			while write_result.nil?
+				selector.select(0)
+			end
+			
 			reader.transfer
+			
+			while read_result.nil?
+				selector.select(0)
+			end
+			
+			expect(write_result).to be == 128
+			expect(read_result).to be == 64
 		end
 		
 		it "can wait for the file to become writable" do
+			wait_result = nil
+			
 			writer = Fiber.new do
-				expect(
-					selector.io_wait(Fiber.current, file, IO::WRITABLE)
-				).to be == IO::WRITABLE
+				wait_result = selector.io_wait(Fiber.current, file, IO::WRITABLE)
 			end
 			
 			writer.transfer
 			
 			selector.select(0)
+			
+			expect(wait_result).to be == IO::WRITABLE
 		end
 	end
 end
