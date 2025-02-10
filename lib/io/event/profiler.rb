e97accff978ce468d9e1d108443f9a2f8049e8d1
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-02-11 00:48:43 +1300

    Add default implementation of `IO::Event::Profiler.default`.

--- lib/io/event/profiler.rb
+++ lib/io/event/profiler.rb
@@ -8,6 +8,11 @@ require_relative "native"
 module IO::Event
 	unless self.const_defined?(:Profiler)
 		module Profiler
+			# The default profiler, if the platform supports it.
+			# Use `IO_EVENT_PROFILER=true` to enable it.
+			def self.default
+				nil
+			end
 		end
 	end
 end
