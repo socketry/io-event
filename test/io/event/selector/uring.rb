2c2be4beeb2370837cf49a8b259826b62d2b01fc
Samuel Williams <samuel.williams@shopify.com>
2026-08-27 13:01:36 +1200

    Fix test formatting
    Assisted-By: devx/efad4cc8-cf12-4d6e-8c55-6f2c3a6683d2

--- test/io/event/selector/uring.rb
+++ test/io/event/selector/uring.rb
@@ -9,14 +9,14 @@ return unless defined?(IO::Event::Selector::URing)
 
 describe IO::Event::Selector::URing do
 	let(:selector) {subject.new(Fiber.current)}
-
+	
 	after do
 		selector&.close
 	end
-
+	
 	it "returns control when submission encounters backpressure" do
 		result, submissions = selector.send(:test_submission_backpressure)
-
+		
 		expect(result).to be == -Errno::EAGAIN::Errno
 		expect(submissions).to be == 1
 	end
